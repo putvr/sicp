ef8b9d3b06bb2990cf579608f476c313a32f6bc4
putvr <putilovroman@gmail.com>
2019-03-10 22:06:57 +0500

    indents fixes

--- chapter 1/code/1.2.2_2.rkt
+++ chapter 1/code/1.2.2_2.rkt
@@ -5,12 +5,12 @@
 
 (define (cc amount kinds-of-coins) 
     (cond ((= amount 0) 1)
-    ((or (< amount 0)
-        (= kinds-of-coins 0)) 0) 
-    (else (+ (cc amount
-                (- kinds-of-coins 1))
-            (cc (- amount
-                    (first-denomination kinds-of-coins))
+     ((or (< amount 0)
+         (= kinds-of-coins 0)) 0) 
+     (else (+ (cc amount
+                 (- kinds-of-coins 1))
+             (cc (- amount
+                     (first-denomination kinds-of-coins))
                  kinds-of-coins)))))
 
 (define (first-denomination kinds-of-coins)
