a50ce1ab5788acf5d4815eaf60ab088e2feb47f1
putvr <putilovroman@gmail.com>
2019-03-14 21:42:30 +0500

    1.1 spaces fix

--- chapter 1/1.1.rkt
+++ chapter 1/1.1.rkt
@@ -19,16 +19,16 @@
 (= a b)
 
 (if (and (> b a) (< b (* a b)))
-b
-a)
+ b
+ a)
 
 (cond ((= a 4) 6)
-((= b 4) (+ 6 7 a))
-(else 25))
+ ((= b 4) (+ 6 7 a))
+ (else 25))
 
 (+ 2 (if (> b a) b a))
 
 (* (cond ((> a b) a)
-((< a b) b)
-(else -1))
-(+ a 1))
+    ((< a b) b)
+    (else -1))
+ (+ a 1))
