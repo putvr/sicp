bf69024a90df9a2b8c6d30926742e6489cb20619
Roman Putilov <Putilov.Roman@gmail.com>
2022-02-22 19:07:55 +0500

    Update exercise 1.8

--- chapter 1/1.8.rkt
+++ chapter 1/1.8.rkt
@@ -1,19 +1,25 @@
-#lang racket 
+#lang racket
+
+(require rackunit)
 
 (define (cbrt-iter guess x)
-    (if (good-enough? guess x)
-        guess
-        (cbrt-iter (improve guess x) x)))
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve guess x) x)))
 
 (define (improve y x)
-    (/ (+ (/ x (* y y)) (* 2 y)) 3))
+  (/ (+ (/ x (* y y)) (* 2 y)) 3))
 
 (define (good-enough? guess x)
-    (< (abs (- (cube guess) x)) 0.001))
+  (< (abs (- (cube guess) x)) 0.0001))
 
 (define (cube x) (* x x x))
 
-(define (cbrt x)
-    (cbrt-iter 1.0 x))
+(define (cube-root x)
+  (cbrt-iter 1.0 x))
 
-(cbrt 27)
+
+(check-equal? (round (* 1000 (cube-root 8.0))) 2000.0)
+(check-equal? (round (* 1000 (cube-root 1000.0))) 10000.0)
+(check-equal? (round (* 1000 (cube-root 1000000000.0))) 1000000.0)
+(check-equal? (round (* 1000 (cube-root 0.008))) 200.0)
